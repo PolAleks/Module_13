1ef73ca4e5bd4f606975a5398bc7b88d267f8acb
Александр Полушкин <aa.polushkin@yandex.ru>
2023-02-11 20:06:25 +0300

    Добавьте файлы проекта.

---
+++ Task_13_1_4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_13_1_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] instance = new int[] {5, 2, 3, 4};
+
+            Console.WriteLine(IsSorted(instance));
+        }
+        static bool IsSorted(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < array[i - 1])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
