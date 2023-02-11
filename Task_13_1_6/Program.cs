6cfe131ac8e39b038a60f64a112f664f5e1ad1bc
Александр Полушкин <aa.polushkin@yandex.ru>
2023-02-11 20:36:07 +0300

    Задание по работе со строками

---
+++ Task_13_1_6/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_13_1_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string pathDestination = Path.Combine(path, "cdev_Text.txt");
+
+            char[] separator  = { ' ', '\r', '\n'};
+
+            string text = File.ReadAllText(pathDestination);
+
+            string[] words = text.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            
+            Console.WriteLine($"Количество слов в файле: {words.Length}");
+        }
+    }
+}
