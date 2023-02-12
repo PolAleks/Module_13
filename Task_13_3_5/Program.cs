47ad212d3e3422121c019f3097ce9c8d64b69562
Александр Полушкин <aa.polushkin@yandex.ru>
2023-02-12 17:38:21 +0300

    Task 13.3.5 complite

---
+++ Task_13_3_5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+namespace Task_13_3_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var months = new List<string>()
+            {
+               "Jan", "Feb", "Mar", "Apr", "May"
+            };
+
+            var missing = new ArrayList()
+            {
+               1, 2, 3, 5, "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+            };
+
+            CombiningColection(months, missing);
+        }
+
+        static void CombiningColection(List<string> list, ArrayList addList)
+        {
+            foreach (var item in addList) {
+                if (item.GetType() == typeof(string))
+                {
+                    list.Add((string)item);
+                }
+            }
+            foreach (var item in list) {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
