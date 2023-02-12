a03cd1f41c07e6cc468509a42aeb1f48bca57207
Александр Полушкин <aa.polushkin@yandex.ru>
2023-02-12 10:57:04 +0300

    Выполнил задание 13.2.5

---
+++ Task_13_2_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+namespace Task_13_2_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var months = new[]
+            {
+               "Jan", "Feb", "Mar", "Apr", "May" , "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+            };
+
+            var numbers = new[]
+            {
+               1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+            };
+            
+            ArrayList array = new ArrayList();
+            
+            for (int i = 0; i < months.Length; i++)
+            {
+                array.Add(months[i]);
+                array.Add(numbers[i]);
+            }
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
