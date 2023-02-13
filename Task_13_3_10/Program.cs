aad06d37d7d721dcf17d76709bb8d90ee4c80102
Александр Полушкин <aa.polushkin@yandex.ru>
2023-02-13 09:25:11 +0300

    Task 13.3.0 complite

---
+++ Task_13_3_10/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task_13_3_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.";
+
+            HashSet<char> set = new HashSet<char>(str.ToHashSet<char>());
+
+            Console.WriteLine(set.Count);
+        }
+    }
+}
---
+++ Task_13_3_8/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task_13_3_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cageOne = new HashSet<string>()
+            {
+               "Волк", "Медведь" , "Тигр", "Кабан"
+            };
+
+            var cageTwo = new HashSet<string>()
+            {
+               "Бык", "Лошадь" , "Баран", "Кабан", "Коза"
+            };
+
+            Console.WriteLine("До удаление Кабана:");
+            PrintCollection(cageOne);
+
+            //cageOne.Remove("Кабан");
+            cageOne.ExceptWith(cageTwo);
+
+            Console.WriteLine("После удаления Кабана:");
+            PrintCollection(cageOne);
+        }
+
+        static void PrintCollection(HashSet<string> collection)
+        {
+            foreach (var item in collection)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
