bf1ecb79e8278218c5b1bc2284304eaa6d921863
Александр Полушкин <aa.polushkin@yandex.ru>
2023-02-13 14:50:34 +0300

    Task 13.4.4 complite

---
+++ Task_13_4_4/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task_13_4_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var phoneBook = new Dictionary<string, Contact>()
+            {
+                { "Игорь", new(79990000000, "igor@example.com") },
+                { "Андрей", new (79990000001, "andrew@example.com") }
+            };
+            PrintPhoneBook(phoneBook);
+
+            phoneBook.TryAdd("Светлана", new Contact(79991234567, "svetlana@ya.ru"));
+            PrintPhoneBook(phoneBook);
+
+            if (phoneBook.TryGetValue("Светлана", out Contact contact))
+                contact.PhoneNumber = 79780203344;
+            PrintPhoneBook(phoneBook);
+
+        }
+
+        static void PrintPhoneBook(Dictionary<string, Contact> phoneBook)
+        {
+            foreach (var contact in phoneBook)
+            {
+                Console.WriteLine($"{contact.Key} - тел.: {contact.Value.PhoneNumber}");
+            }
+            Console.WriteLine(new string('-', 50));
+        }
+    }
+}
