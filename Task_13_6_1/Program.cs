ef92e8fedfee5d78408a529656a0f2319780a91c
Александр Полушкин <aa.polushkin@yandex.ru>
2023-02-16 16:32:20 +0300

    Task 13.6.1 complite

---
+++ Task_13_6_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace Task_13_6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"Text1.txt";
+
+            char[] separator = { ' ', '\r', '\n' };
+
+            string text = File.ReadAllText(path);
+
+            string[] words = text.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> list = new List<string>();
+
+            var stopWatch = Stopwatch.StartNew();
+            foreach ( string word in words )
+            {
+                list.Insert(0, word);
+            }
+            Console.WriteLine($"Время заполнения коллекции List<T> методом вставки: {stopWatch.Elapsed.TotalSeconds:f5}");
+
+            LinkedList<string> linkedList = new LinkedList<string>();
+
+            stopWatch.Restart();
+            foreach ( string word in words )
+            {
+                linkedList.AddFirst(word);
+            }
+            Console.WriteLine($"Время заполнения коллекции LinkedList<T> методом вставки: {stopWatch.Elapsed.TotalSeconds:f5}");
+        }
+    }
+}
